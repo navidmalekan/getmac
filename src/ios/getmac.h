32a853f2b21b884500276124b510126ee153eb07
navidmalekan <navid@malekan.net>
2016-05-04 12:02:54 +0430

    Initial Commit

---
+++ src/ios/getmac.h
@@ -0,0 +1,15 @@
+//
+//  getmac.h
+//  getmac
+//
+//  Created by Admin on 04/04/13.
+//
+//
+
+#import <Cordova/CDV.h>
+
+@interface getmac : CDVPlugin
+
+- (void)getMacAddress:(CDVInvokedUrlCommand*)command;
+
+@end
